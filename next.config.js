f45e17e6a38450ed07353466203f56286a7dc45c
CLOVIS SFEIR <clovis.sfeir.cs@gmail.com>
2025-05-06 15:26:28 +0200

    flutter

--- next.config.js
+++ next.config.js
@@ -1,42 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/PORTEFOLIO-1' : '',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'play-lh.googleusercontent.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'fenelon-notredame.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn-icons-png.flaticon.com',
-        pathname: '/**',
-      }
-    ],
   },
+  basePath: '/PORTEFOLIO',
+  assetPrefix: '/PORTEFOLIO/',
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'avatars.githubusercontent.com',
+      pathname: '/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'placehold.co',
+      pathname: '/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'encrypted-tbn0.gstatic.com',
+      pathname: '/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'play-lh.googleusercontent.com',
+      pathname: '/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'fenelon-notredame.com',
+      pathname: '/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'cdn-icons-png.flaticon.com',
+      pathname: '/**',
+    }
+  ],
   typescript: {
     ignoreBuildErrors: true
   },
